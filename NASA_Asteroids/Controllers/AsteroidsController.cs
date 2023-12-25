c2f38cd038b75c1531cfd44ac19afb488a2d0e10
nmaciasCR <nicolasmacias@hotmail.com>
2023-12-25 18:19:47 +0100

    Crea proyecto NADA Asteroids

---
+++ NASA_Asteroids/Controllers/AsteroidsController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace NASA_Asteroids.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AsteroidsController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult Get([Required(ErrorMessage = "El parámetro 'days' es obligatorio")]
+                                [Range(1, 7, ErrorMessage = "El parámetro 'days' debe ser un valor entre 1 y 7")]
+                                int? days)
+        {
+            return StatusCode(StatusCodes.Status200OK, days);
+        }
+    }
+}
